002e52cf6f514195c97533cf5ab305e0674440f1
Shamanovi4 <roma.chernitskyi@gmail.com>
2021-05-01 01:21:16 +0300

    Created unit testing env. Added tests for Router and createStore

---
+++ src/core/routes/Router.test.js
@@ -0,0 +1,33 @@
+import {Router} from '@core/routes/Router'
+import {Page} from '@core/Page'
+
+class DashboardPage extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'dashboard'
+    return root
+  }
+}
+class SpreadsheetPage extends Page {}
+
+describe('Router:', () => {
+  let router
+  let $root
+
+  beforeEach(() => {
+    $root = document.createElement('div')
+    router = new Router($root, {
+      dashboard: DashboardPage,
+      spreadsheet: SpreadsheetPage
+    })
+  })
+
+  test('should be defined', () => {
+    expect(router).toBeDefined()
+  })
+
+  test('should render Dashboard Page', () => {
+    router.changePageHandler()
+    expect($root.innerHTML).toBe('<div>dashboard</div>')
+  })
+})
